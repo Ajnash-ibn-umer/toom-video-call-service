6a39beda79cce236ec85f1e37b4e3c61a57c6016
ajnash <ajnash.k02@gmail.com>
2023-07-31 01:28:07 +0530

    :

---
+++ src/app/[roomId]/page.tsx
@@ -0,0 +1,248 @@
+"use client"
+
+import Image from 'next/image'
+import styles from './main.module.css'
+import { useEffect, useRef, useState } from 'react'
+import { createChannel, createClient } from 'agora-rtm-react'
+import { useRouter } from 'next/navigation'
+import { BsFillMicFill, BsFillCameraVideoFill, BsFillTelephoneFill ,BsFillMicMuteFill,BsFillCameraVideoOffFill } from 'react-icons/bs'
+
+const APP_ID = "08a276257c5141d1abdbdf81b4bc016d"
+const useClient = createClient(APP_ID);
+let token: string;
+// create ICE  server
+const servers: RTCConfiguration = {
+    iceServers: [
+        {
+            urls: [
+                "stun:stun1.l.google.com:19302",
+                "stun:stun2.l.google.com:19302",
+            ], // free STUN servers provided by Google
+        },
+    ],
+
+};
+export default function Room({ params }: any) {
+
+    const [userId, setUserId] = useState<string>((Math.floor(Math.random() * 1000).toString()))
+    const router = useRouter()
+    const client = useClient();
+    const roomId = params.roomId
+    const [micEnabled, setMicEnabled] = useState<boolean>(true)
+    const [cameraEnabled, setCameraEnabled] = useState<boolean>(true)
+
+    console.log('roomid', roomId);
+
+    const useChannel = createChannel(roomId)
+
+    const channel = useChannel(client)
+
+    //initialze peerconnection
+    let peerConnection = new RTCPeerConnection(servers)
+    console.log('peer connection');
+    // const [localStream, setLocalStream] = useState<any>()
+    // const [remoteStream, setRemoteStream] = useState<any>()
+    const videoRef = useRef<any>()
+    const remoteVideoRef = useRef<any>()
+
+    let localStream: MediaStream;
+    let remoteStream: MediaStream;
+
+
+
+    const getVideo = async () => {
+        localStream = await navigator.mediaDevices
+            .getUserMedia({ video: true, audio: true })
+
+        // setLocallocalStream(localStream)
+        let video = videoRef.current;
+        video.srcObject = localStream;
+
+
+    };
+    useEffect(() => {
+
+        client.login({ uid: userId, token }).then(async () => {
+            await channel.join()
+
+            channel.on("MemberJoined", handleUserJoined)
+            client.on("MessageFromPeer", handleMessageFromPeer)
+            channel.on("MemberLeft", handleMemberLeft)
+            // get media devices access
+            getVideo();
+
+        })
+
+
+
+    }, [videoRef]);
+
+    const handleUserJoined = async (memberId: any) => {
+        console.log("new User joined", memberId);
+
+        await createOffer(memberId)
+
+
+    }
+    const handleMemberLeft = async (memberId: any) => {
+        remoteVideoRef.current.style.display = "none"
+    }
+    const handleMessageFromPeer = async (message: any, memberId: string) => {
+        // console.log("Message:", message, memberId)
+
+        message = JSON.parse(message.text)
+        console.log("Message:", message, memberId)
+
+        // catch offer from another user and create answer
+        if (message.type === "offer") {
+            await createAnswer(memberId, message.offer)
+        }
+
+        if (message.type === "answer") {
+            await addAnswer(message.answer)
+        }
+
+        if (message.type === "candidate") {
+            if (peerConnection) {
+                peerConnection.addIceCandidate(message.candidate)
+            }
+        }
+    }
+
+    // create connection function
+    const createConnection = (memberId: any) => {
+
+
+        // set remote stream
+        remoteStream = new MediaStream()
+        var video = remoteVideoRef.current;
+        video.srcObject = remoteStream;
+        remoteVideoRef.current.style.display = "block"
+        // add localstram to peer connection
+        console.log(' remote configured');
+        console.log("tracks", localStream.getTracks());
+
+        // handle initial error , when local stream is null
+        if (!localStream) {
+            getVideo()
+        }
+
+        localStream.getTracks().forEach((track) => {
+            console.log('locla track', track);
+
+            peerConnection.addTrack(track, localStream)
+        })
+
+        // get remote streams
+        peerConnection.ontrack = (event) => {
+            console.log({ event });
+
+            event.streams[0]?.getTracks().forEach((track: any) => {
+                remoteStream.addTrack(track)
+            })
+        }
+        console.log('new ice');
+        peerConnection.onicecandidate = async (event) => {
+            console.log('new ice');
+
+            if (event.candidate) {
+                console.log('new ICE candidate', event.candidate);
+                client.sendMessageToPeer({ text: JSON.stringify({ "type": "candidate", "candidate": event.candidate }) }, memberId)
+
+
+            } else {
+                console.log('ice candidate  not found',);
+
+            }
+        }
+    }
+    const createOffer = async (memberId: any) => {
+
+        await createConnection(memberId)
+        // create offer
+        let offer = await peerConnection.createOffer()
+        // this contains sdp
+        await peerConnection.setLocalDescription(offer)
+        console.log('offer :', offer);
+        client.sendMessageToPeer({ text: JSON.stringify({ "type": "offer", offer }) }, memberId)
+
+    }
+
+    const createAnswer = async (memberId: string, offer: any) => {
+        await createConnection(memberId)
+        await peerConnection.setRemoteDescription(offer)
+
+        let answer = await peerConnection.createAnswer()
+        // this contains sdp
+        await peerConnection.setLocalDescription(answer)
+        console.log('offer :', offer);
+        client.sendMessageToPeer({ text: JSON.stringify({ "type": "answer", answer }) }, memberId)
+    }
+
+    const addAnswer = async (answer: any) => {
+        if (!peerConnection.currentRemoteDescription) {
+            peerConnection.setRemoteDescription(answer)
+        }
+    }
+
+
+    const leaveChannel = async () => {
+
+        await channel.leave()
+        await client.logout()
+    }
+
+
+    const handleCamera = () => {
+        var videoTrack: any =  localStream?.getTracks().find(track => track.kind === "video")
+        // if(videoTrack?.enabled !== undefined) 
+
+        if (videoTrack?.enabled ) {
+
+            videoTrack.enabled = false;
+            // micEnabled=false
+
+        } else {
+            videoTrack.enabled = true;
+            // setCameraEnabled(true)
+
+
+        }
+    }
+    const handleMic = () => {
+        var audioTrack: any = localStream?.getTracks().find(track => track.kind === "audio")
+// if(audioTrack?.enabled !== undefined) return
+        if (audioTrack?.enabled ) {
+            audioTrack.enabled = false;
+            // setMicEnabled(false)
+
+        } else {
+            audioTrack.enabled = true;
+            // setMicEnabled(true)
+
+
+        }
+    }
+    const handleEndCall = () => {
+        leaveChannel()
+        router.push('/')
+    }
+    window.addEventListener("beforeunload", leaveChannel)
+    return (
+        <main className={styles.main}>
+            <div className={styles.videos} id='videos'>
+
+                <video ref={videoRef} className={`${styles.videoPlayer} ${styles.user1}`} id='user1' autoPlay playsInline ></video>
+                <video ref={remoteVideoRef} className={`${styles.videoPlayer} ${styles.user2}`} id='user2' autoPlay playsInline ></video>
+
+            </div>
+            <div className={styles.controls}>
+                <div className={styles.controlBtn} onClick={handleCamera}  >{ cameraEnabled ? <BsFillCameraVideoFill size={30} /> :<BsFillCameraVideoOffFill size={30} />}</div>
+                <div className={styles.controlBtn} onClick={handleMic}  >{ micEnabled? <BsFillMicFill size={30} /> :<BsFillMicMuteFill size={30} />}</div>
+                <div className={styles.controlBtn} onClick={handleEndCall} style={{ background: "red" }} ><BsFillTelephoneFill size={30} /></div>
+
+
+            </div>
+        </main>
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,122 +1,211 @@
 "use client"
 
-import Image from 'next/image'
+import { Fragment } from 'react'
+import { Disclosure, Menu, Transition } from '@headlessui/react'
 import styles from './main.module.css'
-import { useEffect, useRef, useState } from 'react'
+import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
+import { useRouter } from 'next/navigation'
 
-// create ICE  server
-const servers: RTCConfiguration = {
-  iceServers: [
-    {
-      urls: [
-        "stun:stun1.l.google.com:19302",
-        "stun:stun2.l.google.com:19302",
-      ], // free STUN servers provided by Google
-    },
-  ],
+const user = {
+  name: 'Tom Cook',
+  email: 'tom@example.com',
+  imageUrl:
+    'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+}
+const navigation = [
+  { name: 'Dashboard', href: '#', current: true },
+  { name: 'Team', href: '#', current: false },
+  { name: 'Projects', href: '#', current: false },
+  { name: 'Calendar', href: '#', current: false },
+  { name: 'Reports', href: '#', current: false },
+]
+const userNavigation = [
+  { name: 'Your Profile', href: '#' },
+  { name: 'Settings', href: '#' },
+  { name: 'Sign out', href: '#' },
+]
 
-};
-export default function Home() {
+function classNames(...classes:any[]) {
+  return classes.filter(Boolean).join(' ')
+}
 
-
-  // const [localStream, setLocalStream] = useState<any>()
-  // const [remoteStream, setRemoteStream] = useState<any>()
-  const videoRef = useRef<any>()
-  const remoteVideoRef = useRef<any>()
-
-  let localStream: MediaStream;
-  let remoteStream: MediaStream;
-
-
-  // useEffect(() => {
-
-  //   (async () => {
-  //     let stream = await navigator.mediaDevices.getDisplayMedia({ audio: false, video: true })
-  //     console.log({stream});
-
-  //     setLocalStream(stream)
-  //     document.getElementById("user1").srcObject = localStream
-  //   })()
-  // }, [])
-
-  useEffect(() => {
-    getVideo();
-  
-
-  }, [videoRef]);
-  
+export default function Main() {
 
 
-  var createOffer = async () => {
-    //initialze peerconnection
-    let peerConnection = new RTCPeerConnection(servers)
-console.log('peer connection');
 
-    // set remote stream
-    remoteStream = new MediaStream()
-    let video = remoteVideoRef.current;
-    video.srcObject = remoteStream;
 
-    // add localstram to peer connection
-    console.log(' remote configured');
-    console.log("tracks",localStream.getTracks());
-    
-    localStream.getTracks().forEach((track) => {
-      console.log('locla track',track);
-      
-      peerConnection.addTrack(track, localStream)
-    })
-
-    // get remote streams
-    peerConnection.ontrack=(event) => {
-      console.log({event});
-      
-      event.streams[0]?.getTracks().forEach((track: any) => {
-      remoteStream.addTrack(track)
-      })
-    }
-    console.log('new ice');
-    peerConnection.onicecandidate=async(event)=>{
-      console.log('new ice');
-      
-      if(event.candidate){
-        console.log('new ICE candidate',event.candidate);
-        
-      }else{
-        console.log('ice candidate  not found',);
-        
-      }
-    }
-
-    // create offer
-    let offer = await peerConnection.createOffer()
-    // this contains sdp
-    await peerConnection.setLocalDescription(offer)
-    console.log('offer :', offer);
-
+  const router = useRouter()
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+   
+    const values:any=e?.target?.roomId.value
+    console.log("my room iD", values);
+    router.push(`/${values}`)
   }
-
-
-  const getVideo = async () => {
-    localStream = await navigator.mediaDevices
-      .getUserMedia({ video: true })
-
-    // setLocallocalStream(localStream)
-    let video = videoRef.current;
-     video.srcObject = localStream;
-
-    createOffer()
-
-  };
-
+  const handleNewMeet=() => {
+    const newRoomId=Math.floor(Math.random()*1000000000)
+    router.push(`/${newRoomId}`)
+   }
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className={styles.videos} id='videos'>
+    <>
+      {/*
+        This example requires updating your template:
 
-        <video ref={videoRef} className={styles.videoPlayer} id='user1' autoPlay playsInline ></video>
-        <video ref={remoteVideoRef} className={styles.videoPlayer} id='user2' autoPlay playsInline ></video>
+        ```
+        <html class="h-full bg-gray-100">
+        <body class="h-full">
+        ```
+      */}
+      <div className="min-h-full">
+        <Disclosure as="nav" className="bg-gray-800">
+          {({ open }) => (
+            <>
+              <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+                <div className="flex h-16 items-center justify-between">
+                  <div className="flex items-center">
+                    <div className="flex-shrink-0">
+                      <h1 className={styles.heading}>Dei ! Meet</h1>
+                    </div>
+                    <div className="hidden md:block">
 
+                    </div>
+                  </div>
+                  <div className="hidden md:block">
+                    <div className="ml-4 flex items-center md:ml-6">
+
+
+                      {/* Profile dropdown */}
+                      <Menu as="div" className="relative ml-3">
+                        <div>
+                          <Menu.Button className="relative flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                            <span className="absolute -inset-1.5" />
+                            <span className="sr-only">Open user menu</span>
+                            <img className="h-8 w-8 rounded-full" src={user.imageUrl} alt="" />
+                          </Menu.Button>
+                        </div>
+                        <Transition
+                          as={Fragment}
+                          enter="transition ease-out duration-100"
+                          enterFrom="transform opacity-0 scale-95"
+                          enterTo="transform opacity-100 scale-100"
+                          leave="transition ease-in duration-75"
+                          leaveFrom="transform opacity-100 scale-100"
+                          leaveTo="transform opacity-0 scale-95"
+                        >
+                          <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                            {userNavigation.map((item) => (
+                              <Menu.Item key={item.name}>
+                                {({ active }) => (
+                                  <a
+                                    href={item.href}
+                                    className={classNames(
+                                      active ? 'bg-gray-100' : '',
+                                      'block px-4 py-2 text-sm text-gray-700'
+                                    )}
+                                  >
+                                    {item.name}
+                                  </a>
+                                )}
+                              </Menu.Item>
+                            ))}
+                          </Menu.Items>
+                        </Transition>
+                      </Menu>
+                    </div>
+                  </div>
+                  <div className="-mr-2 flex md:hidden">
+                    {/* Mobile menu button */}
+                    <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md bg-gray-800 p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                      <span className="absolute -inset-0.5" />
+                      <span className="sr-only">Open main menu</span>
+                      {open ? (
+                        <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
+                      ) : (
+                        <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
+                      )}
+                    </Disclosure.Button>
+                  </div>
+                </div>
+              </div>
+
+              <Disclosure.Panel className="md:hidden">
+
+                <div className="border-t border-gray-700 pb-3 pt-4">
+                  <div className="flex items-center px-5">
+                    <div className="flex-shrink-0">
+                      <img className="h-10 w-10 rounded-full" src={user.imageUrl} alt="" />
+                    </div>
+                    <div className="ml-3">
+                      <div className="text-base font-medium leading-none text-white">{user.name}</div>
+                      <div className="text-sm font-medium leading-none text-gray-400">{user.email}</div>
+                    </div>
+                    <button
+                      type="button"
+                      className="relative ml-auto flex-shrink-0 rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+                    >
+                      <span className="absolute -inset-1.5" />
+                      <span className="sr-only">View notifications</span>
+                      <BellIcon className="h-6 w-6" aria-hidden="true" />
+                    </button>
+                  </div>
+                  <div className="mt-3 space-y-1 px-2">
+                    {userNavigation.map((item) => (
+                      <Disclosure.Button
+                        key={item.name}
+                        as="a"
+                        href={item.href}
+                        className="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white"
+                      >
+                        {item.name}
+                      </Disclosure.Button>
+                    ))}
+                  </div>
+                </div>
+              </Disclosure.Panel>
+            </>
+          )}
+        </Disclosure>
+
+
+        <main className='flex' >
+      
+          <div className={styles.leftDiv}>
+          <button  onClick={handleNewMeet} className="bg-green-600  hover:bg-green-700 text-white font-bold  rounded">
+                  + New Meeting
+                </button>
+            <form onSubmit={handleSubmit}>
+              <div className={styles.forms}>
+
+                <div>
+                  <label htmlFor="price" className="block text-sm font-medium leading-6 text-gray-900">
+                    Room ID
+                  </label>
+                  <div className="relative mt-2 rounded-md shadow-sm">
+
+                    <input
+                      type="text"
+                      name="roomId"
+                      id="roomId"
+                      className="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+
+                    />
+
+                  </div>
+                </div>
+                <button type="submit" className="bg-blue-500  hover:bg-blue-700 text-white font-bold  rounded">
+                  Join
+                </button>
+                <div>
+
+                </div>
+              </div>
+            </form>
+
+
+          </div>
+
+        </main>
       </div>
-    </main>
+    </>
   )
 }
