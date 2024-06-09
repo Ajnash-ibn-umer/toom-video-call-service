08f1859506580f8e11deb37cd1d161ae541119e1
ajnash-credot <108794235+ajnash-credot@users.noreply.github.com>
2024-06-09 19:43:02 +0530

    firebase reverted

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,21 +5,13 @@ import { Disclosure, Menu, Transition } from "@headlessui/react";
 import styles from "./main.module.css";
 import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
 import { useRouter } from "next/navigation";
-import TopBar from "@/components/NavBar";
-import LandingPage from "@/components/LandingPage";
+import TopBar from "@/app/components/NavBar";
+import LandingPage from "@/app/components/LandingPage";
 import { initializeApp } from "firebase/app";
 // function classNames(...classes: any[]) {
 //   return classes.filter(Boolean).join(" ");
 // }
-const firebaseConfig = {
-  apiKey: "AIzaSyDeqfWAorFFj_bI3UJ1yks6LxFUw4oWdZQ",
-  authDomain: "webrtc-eefbf.firebaseapp.com",
-  projectId: "webrtc-eefbf",
-  storageBucket: "webrtc-eefbf.appspot.com",
-  messagingSenderId: "357808059385",
-  appId: "1:357808059385:web:bf4d9a3aa03d4b2020c328"
-};
-export const firebaseApp = initializeApp(firebaseConfig);
+
 export default function Main() {
   const router = useRouter();
   const handleSubmit = (e: any) => {
